f0d0f482f5e4fb4c8101bbd2a998540a0a503859
Krzysztof Gronski <krzysztof.gronski.pl@gmail.com>
2022-11-12 22:02:34 +0100

    add remove task

--- js/script.js
+++ js/script.js
@@ -12,6 +12,11 @@ const addNewTask=(newTaskContent)=>{
     render();
 };
 
+const removeTask(index){
+    tasks.splice(index,1);
+            render();
+};
+
 const onFormSubmit=(event)=>{
 
     event.preventDefault();
@@ -28,11 +33,19 @@ const render = () => {
         htmlString+=`
         <li class="ul__li${task.done?"--done\"":"\""}>  
         ${task.content}
+        <button class="js-remove">Usun</button>
         </li>
         `;
     }
 
     document.querySelector(".js-tasks").innerHTML=htmlString;
+    const removeButtons = document.querySelectorAll(".js-remove";)
+    removeButtons.forEach((removeButton,index)=>{
+        removeButton.addEventListener("click",()=>{
+            removeTask(index);
+        })
+
+    })
 };
 
 const init = () => {
