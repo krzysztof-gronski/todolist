103dc77aac5ae1bb0be252503e5607c7ec1965c0
Krzysztof Gronski <krzysztof.gronski.pl@gmail.com>
2022-11-12 20:36:14 +0100

    adding tasks

--- js/script.js
+++ js/script.js
@@ -1,24 +1,45 @@
 {
 
-    const tasks = [];
+    const tasks = [
+        { content: "dede",
+        done: true},
+        {content: "jrifrjf",
+        done: false},
+    ];
 
+const addNewTask=(newTaskContent)=>{
+    tasks.push({content: newTaskContent});
+    render();
+};
 
+const onFormSubmit=(event)=>{
+
+    event.preventDefault();
+    const newTaskContent=document.querySelector(".js-newTask").value.trim();
+    if(newTaskContent===""){
+        return;
+    }
+    addNewTask(newTaskContent);
+}
 
 const render = () => {
     let htmlString = "";
     for(const task of tasks){
         htmlString+=`
-        <li>  
+        <li class="ul__li${task.done?"--done\"":"\""}>  
         ${task.content}
         </li>
         `;
     }
 
     document.querySelector(".js-tasks").innerHTML=htmlString;
-}
+};
 
 const init = () => {
     render();
+
+    const form=document.querySelector(".js-form");
+    form.addEventListener("submit",onFormSubmit);
 };
 
 init();
