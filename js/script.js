82f0b52a2fc35fbd488b7bc4bd9bb1dc01f2e95b
Krzysztof Gronski <krzysztof.gronski.pl@gmail.com>
2022-11-13 00:27:23 +0100

    styled block elements

--- js/script.js
+++ js/script.js
@@ -1,73 +1,85 @@
 {
 
-    const tasks = [
-        { content: "dede",
-        done: true},
-        {content: "jrifrjf",
-        done: false},
-    ];
+    const tasks = [];
 
-const addNewTask=(newTaskContent)=>{
-    tasks.push({content: newTaskContent});
-    render();
-};
+    const addNewTask = (newTaskContent) => {
+        tasks.push({ content: newTaskContent });
+        render();
+    };
 
-const removeTask = (index)=>{
-    tasks.splice(index,1);
-            render();
-};
+    const removeTask = (index) => {
+        tasks.splice(index, 1);
+        render();
+    };
 
-const toggleTaskDone=(index)=>{
-    tasks[index].done=!tasks[index].done;
-            render();
-};
+    const toggleTaskDone = (index) => {
+        tasks[index].done = !tasks[index].done;
+        render();
+    };
 
-const onFormSubmit=(event)=>{
+    const bindEvents = () => {
+        const removeButtons = document.querySelectorAll(".js-remove");
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            })
 
-    event.preventDefault();
-    const newTaskContent=document.querySelector(".js-newTask").value.trim();
-    if(newTaskContent===""){
-        return;
+        })
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+        toggleDoneButtons.forEach((toggleDoneButton, index) => {
+            toggleDoneButton.addEventListener("click", () => {
+                toggleTaskDone(index);
+            })
+
+        });
+    };
+
+    const onFormSubmit = (event) => {
+
+        event.preventDefault();
+        const newTaskInput = document.querySelector(".js-newTask");
+        const newTaskContent = newTaskInput.value.trim();
+        if (newTaskContent === "") {
+            newTaskInput.value = "";
+            newTaskInput.focus();
+            return;
+        }
+        addNewTask(newTaskContent);
+        newTaskInput.value = "";
+        newTaskInput.focus();
     }
-    addNewTask(newTaskContent);
-}
 
-const render = () => {
-    let htmlString = "";
-    for(const task of tasks){
-        htmlString+=`
-        <li class="ul__li${task.done?"--done\"":"\""}>  
-        <button class="js-done">done</button>
+    const render = () => {
+        let htmlString = "";
+        for (const task of tasks) {
+            htmlString += `
+        <li class="ul__li${task.done ? "--done\"" : "\""}>  
+        <button class="ul__button js-done"></button>
         ${task.content}
-        <button class="js-remove">Usun</button>
+        <button class="ul__button ul__button--remove js-remove">X</button>
         </li>
         `;
-    }
+        }
 
-    document.querySelector(".js-tasks").innerHTML=htmlString;
-    const removeButtons = document.querySelectorAll(".js-remove";)
-    removeButtons.forEach((removeButton,index)=>{
-        removeButton.addEventListener("click",()=>{
-            removeTask(index);
-        })
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+        bindEvents();
 
-    })
-    const toggleDoneButtons = document.querySelectorAll(".js-done";)
-    toggleDoneButtons.forEach((toggleDoneButton,index)=>{
-        toggleDoneButton.addEventListener("click",()=>{
-            toggleTaskDone(index);
-        })
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+        toggleDoneButtons.forEach((toggleDoneButton, index) => {
+            if(tasks[index].done){toggleDoneButton.textContent="V"};
 
-    })
-};
+        });
 
-const init = () => {
-    render();
 
-    const form=document.querySelector(".js-form");
-    form.addEventListener("submit",onFormSubmit);
-};
+    };
 
-init();
+    const init = () => {
+        render();
+
+        const form = document.querySelector(".js-form");
+        form.addEventListener("submit", onFormSubmit);
+    };
+
+    init();
 
 }
